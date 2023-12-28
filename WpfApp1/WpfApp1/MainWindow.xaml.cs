2113656afc17f97cacccbe282d3f1533a5c437ef
Ela1009 <ela.vrcelj@gmail.com>
2023-12-28 11:30:24 +0100

    Intro_Project_Cycle2

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace StopwatchApp
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+        private TimeSpan elapsedTime;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            elapsedTime = elapsedTime.Add(TimeSpan.FromSeconds(1));
+            UpdateElapsedTime();
+        }
+
+        private void UpdateElapsedTime()
+        {
+            stopwatchText.Text = elapsedTime.ToString(@"hh\:mm\:ss");
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            elapsedTime = TimeSpan.Zero;
+            UpdateElapsedTime();
+        }
+    }
+}
